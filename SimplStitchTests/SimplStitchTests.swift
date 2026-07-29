134e7c44dab4cbb6961c96b05c10d4b6dfaf9b63
Marc <einlama@gmail.com>
2026-07-29 09:31:33 +0200

    Initial Commit

---
+++ SimplStitchTests/SimplStitchTests.swift
@@ -0,0 +1,19 @@
+//
+//  SimplStitchTests.swift
+//  SimplStitchTests
+//
+//  Created by Marc Brechbühl on 29.07.2026.
+//
+
+import Testing
+@testable import SimplStitch
+
+struct SimplStitchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
